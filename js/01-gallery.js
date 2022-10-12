6a22e61a8326b8d59dfce89c2cffa7863a27af08
SychTim <sychtim@gmail.com>
2022-10-12 19:13:02 +0200

    подключает библиотеку и реализует ее работу

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,9 +6,24 @@ const gallery = document.querySelector('.gallery');
 let string = galleryItems.map(array => {
         return `<div class="gallery__item">
     <img class="gallery__image gallery__link"
+    data-source="${array.original}"
     src="${array.preview}"
     alt="${array.description}">
 </div>`;
 }).join('');
 
 gallery.innerHTML = string;
+
+gallery.addEventListener('click', openModal);
+
+function openModal(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="600" height="800">
+    `);
+
+    instance.show();
+}
