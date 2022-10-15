20b2c87494f25f9137d39c52ad5d486f83c4acce
SychTim <sychtim@gmail.com>
2022-10-15 15:49:36 +0200

    реализует работу библиотеки

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryItems.map(array => {
+        return `<a class="gallery__item" href="${array.preview}">
+  <img class="gallery__image" src="${array.original}" alt="${array.description}" />
+</a>`;
+
+}).join('');
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
